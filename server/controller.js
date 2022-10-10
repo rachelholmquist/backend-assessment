dd05bca10c0674f88cb6050f0a47f86eec3afede
Rachel Holmquist <rachelflorek18@outlook.com>
2022-10-10 10:28:19 -0700

    axios not working

---
+++ server/controller.js
@@ -0,0 +1,22 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["All the effort you are making will ultimately pay off.", "If you wish to see the best in others, show the best of yourself.", "Place special emphasis on old friendship.", "We first make our habits, and then our habits make us."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * fortunes.length);
+        let randomFortune = fortunes[randomIndex];
+      
+        res.status(200).send(randomFortune);
+    }
+
+}
